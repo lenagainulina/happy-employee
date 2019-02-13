802ad29d41f68bc66115f92f11b80191aae43c4a
lenagainulina <aoneko@gmx.de>
2019-02-13 10:05:24 +0100

    Initial scaffolding

---
+++ src/main/java/com/takeaway/happyemployee/HappyEmployeeApplication.java
@@ -0,0 +1,14 @@
+package com.takeaway.happyemployee;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HappyEmployeeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HappyEmployeeApplication.class, args);
+	}
+
+}
+
